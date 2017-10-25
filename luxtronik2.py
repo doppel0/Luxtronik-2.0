effb0810ff9ab458206a5dc06331939923a359ce
doppel0 <33105352+doppel0@users.noreply.github.com>
2017-10-26 01:25:41 +0200

    Update luxtronik2.py

--- luxtronik2.py
+++ luxtronik2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Script to read values from Luxtronik 2.0 Heat Pump systems (Alpha Innotec and other vendors)
+# Script to read values from Luxtronik 2.0 heat pump control units (used by Alpha Innotec and other vendors)
 # Temperature values are displayed in degrees Celsius
 #
 # by Stefan Prokop
